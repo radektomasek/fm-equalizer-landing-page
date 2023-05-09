283a5149e1682b42d7cc908a93658748a3f15951
Radek Tomasek <radek.tomasek@gmail.com>
2023-05-09 07:09:58 +0100

    Added more Tailwind configuration + basic backgrounds

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '375px',
+        md: '768px',
+        lg: '1440px',
+      },
+      colors: {
+        cyan: 'hsl(177, 68%, 64%)',
+        lightRed: 'hsl(12, 94%, 65%)',
+        orange: 'hsl(33, 100%, 70%)',
+        lightGray: 'hsl(20, 33%, 98%)',
+        darkBlue: 'hsl(244, 23%, 12%)'
+      },
+      fontFamily: {
+        plex: ['IBM Plex Sans', 'sans-serif'],
+      },
+      backgroundImage: {
+        'small': "url('../../assets/bg-main-mobile.png')",
+        'medium': "url('../../assets/bg-main-tablet.png')",
+        'large': "url('../../assets/bg-main-desktop.png')",
+      },
+    },
   },
   plugins: [],
 }
